661e0e9a8f0f003627bd834e98646859f3cdd856
Andrew Kwong <kwong.andy93@gmail.com>
2020-10-21 16:58:55 -0700

    Passes tests for reducers

--- src/reducers/currentVideo.js
+++ src/reducers/currentVideo.js
@@ -1,7 +1,20 @@
 import Redux from 'redux';
-
+//TODO: define a reducer for the currentVideo field of our state.
 var currentVideoReducer = (state, action) => {
-  //TODO: define a reducer for the currentVideo field of our state.
+
+  switch (action.type) {
+  case 'CHANGE_VIDEO':
+    let result = {};
+    if (state !== undefined) {
+      result = { ...state };
+      result.currentVideo = action.video;
+      return result;
+    } else {
+      return state = action.video;
+    }
+  default:
+    return null;
+  }
 };
 
 export default currentVideoReducer;
