5995cd822d2ce3f58926fbe53cecc292d7987d3a
Andrew Kwong <kwong.andy93@gmail.com>
2020-10-22 09:23:24 -0700

    VideoList and VideoPlayer containers

--- src/containers/VideoPlayerContainer.js
+++ src/containers/VideoPlayerContainer.js
@@ -1,8 +1,18 @@
 import { connect } from 'react-redux';
 import VideoPlayer from '../components/VideoPlayer.js';
 
-var VideoPlayerContainer = () => {};
 
+var mapStateToProps = (state) => {
+  return {
+    video: state.currentVideo,
+  };
+};
+
+const mapDispatchToProps = {};
+
+//onClick = { props.handleSearchChange }
+
+var VideoPlayerContainer = connect(mapStateToProps, mapDispatchToProps)(VideoPlayer);
 //TODO: define a VideoPlayerContainer component which will hook up your action
 //dispatchers with your VideoPlayer component props.
 
